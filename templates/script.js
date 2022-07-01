d1f9e9774d7f53dd894ce611dd3c47bc17f05ad7
nick <cnickm@gmail.com>
2022-06-30 19:05:59 -0700

    initial calc commit

---
+++ templates/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $("#calculate").click(function(){
+        var everclear = $("#everclear").val();
+        var intEverclear = parseFloat(everclear);
+        var final = $("#final").val();
+        var intFinal = parseFloat(final);
+        var volume = $("#volume").val();
+        var intVolume = parseFloat(volume);
+        var answer = (intVolume * intFinal) / intEverclear;
+        $("#answer").html(answer)
+    });
+
+});
